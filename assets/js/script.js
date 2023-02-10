1c07ab3816008c7ce358e2c49c8fa1ad0d52055d
AntonioRobledo <robledoy@uci.edu>
2023-02-09 16:49:14 -0800

    cleaned up the javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
+// Event listener for clicking the save button and storing user input into local storage
+
   var saveButton = $('.saveBtn');
     saveButton.on('click', function() {
       var userInput = $(this).siblings(".description").val();
@@ -14,11 +7,8 @@
       localStorage.setItem(currentHour,userInput);
     });
     
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+    // applying jquery $(this) method to manipulate time-block colors according 
+    // to past, present, or future tasks
     
     function time() {
       var currentTime = dayjs().format('HH');
@@ -48,12 +38,11 @@
   
   time();
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+  // dayjs()hour() reads as 24 hour clock so 1PM = 13th hour
+  
+  // takes the value of whatever is int he description box and 
+  // saves that into local storage 
 
-    // dayjs()hour() reads as 24 hour clock so 1PM = 13th hour
-    
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -64,6 +53,7 @@
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-    // TODO: Add code to display the current date in the header of the page.
+  // formats and displays the month, day, and year
+
 var currentDate = dayjs();
 $('#currentDay').text(currentDate.format('MMMM D, YYYY'))
